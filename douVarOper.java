e66565f4569ee36b1beac04f684403965b7862c5
Tulsi-Sharma3 <tulsisharma7069@gmail.com>
2026-01-29 23:02:21 +0530

    Double variable no., performing some operations

---
+++ douVarOper.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class douVarOper {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("INPUT : ");
+        System.out.println("Enter the num : ");
+        double num = sc.nextDouble();
+        int numInteger = (int) num;
+        System.out.println("numRound : " + Math.round(num));
+        System.out.println("numCeil : " + Math.ceil(num));
+        System.out.println("numFloor: " + Math.floor(num));
+        System.out.println("numInteger : " + numInteger);
+    }
+}
