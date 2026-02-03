fe5fbf133d3134a5a8ed46918a0e958530b428ef
Tulsi-Sharma3 <tulsisharma7069@gmail.com>
2026-02-03 23:54:54 +0530

    Floy'd triangle

---
+++ Secondpattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Secondpattern {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=5;
+        int num = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(" " + num + " " );
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+
