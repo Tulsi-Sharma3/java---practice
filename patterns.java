81ab81b7fc3e858ff05300e7f9d3f220e52d031b
Tulsi-Sharma3 <tulsisharma7069@gmail.com>
2026-02-03 23:43:58 +0530

    printing pattern

---
+++ patterns.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class patterns {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = 4;
+        int m=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
