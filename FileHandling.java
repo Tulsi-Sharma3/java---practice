2e4ed7fa6cf9f0d577b2c69c3031b0db8bb42e8b
Tulsi-Sharma3 <tulsisharma7069@gmail.com>
2026-03-07 23:45:51 +0530

    Exception handling using try & catch

---
+++ FileHandling.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FileHandling {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        try{
+            int  a = 10/0;
+        }
+        catch(ArithmeticException e){
+            System.out.println("Division by 0 error");
+        }
+    }
+}
