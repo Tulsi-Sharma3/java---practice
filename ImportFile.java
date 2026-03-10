c2719d7d69ba962466a2636b5b2a1b1cd04f4978
Tulsi-Sharma3 <tulsisharma7069@gmail.com>
2026-03-10 23:43:15 +0530

    file creation

---
+++ ImportFile.java
@@ -0,0 +1,7 @@
+import java.io.File;
+public class ImportFile {
+    public static void main(String[] args){
+        File obj = new File("myFile.txt");
+        System.out.println("File created");
+    }
+}
