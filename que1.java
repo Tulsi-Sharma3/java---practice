c9b1888f341537d0675b71b7a177f9306eedaf7b
Tulsi-Sharma3 <tulsisharma7069@gmail.com>
2026-03-19 00:17:52 +0530

    smallest value in an array

--- que1.java
+++ que1.java
@@ -1,7 +1,20 @@
 import java.util.*;
+
+
 public class que1{
      public static void main(String[] args) {
-         System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[] = {47,26,33,93,78};
+        int size = 5;
+        int smallest = Integer.MAX_VALUE;
+        for(int i=0; i<size; i++){
+            if(arr[i]<smallest){
+                smallest = arr[i];
 
+            }
+
+        }
+        System.out.println(smallest);
     }
 }
